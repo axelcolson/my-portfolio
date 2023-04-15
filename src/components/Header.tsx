e915a7c6553052015265a33b18f610b61846b633
Axel Colson <axel_colson@hotmail.com>
2023-04-15 10:49:07 +0200

    Added link in the top left corner. Changed color palette. added white square behind picture

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -14,11 +14,13 @@ const Header: React.FC<HeaderProps> = (props) => {
 
   return (
     <header className="header">
+      <a href="#" id='home-btn'>axelcolson</a>
       <div id='left-side-header'>
         <div id='left-side-container'>
             <h1 id='job-title'>Full Stack Developer</h1>
             <p id='job-sub-p'>Blending the art of front-end design with the technical precision of back-end development.</p>
             <div id='profile-img-container'></div>
+            <div id='floating-square'></div>
         </div>
       </div>
       <div id='right-side-header'></div>
