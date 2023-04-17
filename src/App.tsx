071fc8e6f969ce811b88a200ba7948c01758cda7
Axel Colson <axel_colson@hotmail.com>
2023-04-17 21:01:08 +0200

    Added custom scrollbar

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import React from 'react';
 import './App.css';
 import { Header } from './components/Header';
+import { Experience } from './components/Experience';
 
 function App() {
   return (
@@ -12,7 +13,12 @@ function App() {
         phone="+32 476 73 37 81"
         linkedIn="https://www.linkedin.com/in/axel-colson-a78814154/"
       />
-      {/* Other components will go here */}
+      <Experience 
+        name="Axel Colson"
+        title="Full-Stack Developer"
+        email="axel_colson@hotmail.com"
+        phone="+32 476 73 37 81"
+        linkedIn="https://www.linkedin.com/in/axel-colson-a78814154/"/>
     </div>
   );
 }
---
+++ src/components/Experience.tsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import '../stylesheets/Experience.css';
+
+interface ExperienceProps {
+    name: string;
+    title: string;
+    email: string;
+    phone: string;
+    linkedIn: string;
+  }
+
+
+const Experience: React.FC<ExperienceProps> = (props) => {
+    const { name, title, email, phone, linkedIn } = props;
+  
+    return (
+      <header className="experience">
+        <div id='experience-container'>
+            <h2 id='experience-title'>
+                <span id='title-sub'>MY</span> EXPERIENCE
+            </h2>
+        </div>
+      </header>
+      
+    );
+  };
+  
+
+export { Experience };
