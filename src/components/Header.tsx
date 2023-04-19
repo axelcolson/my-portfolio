3b12c524a11b7a46ae95811d4c745c185201cb0b
Axel Colson <axel_colson@hotmail.com>
2023-04-19 21:01:24 +0200

    Added media screens for smaller displays in App + Header

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -25,11 +25,12 @@ const Header: React.FC<HeaderProps> = (props) => {
                 <a href='https://www.linkedin.com/in/axel-colson-a78814154/' target='_blank' className='header-button' id='learn-more-btn-header' rel="noreferrer">Learn more</a>
                 <a href='mailto:axel_colson@hotmail.com' className='header-button' id='contact-me-btn-header'>Contact me</a>
               </div>
-              <div id='profile-img-container'></div>
-              <div id='floating-square'></div>
           </div>
         </div>
-        <div id='right-side-header'></div>
+        <div id='right-side-header'>
+          <div id='profile-img-container'></div>
+          <div id='floating-square'></div>
+        </div>
       </div>
     </header>
     
